66fb980487a6c1a15e64b74f3e34882edbd21b9f
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-02 13:20:00 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
@@ -15,11 +16,11 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/profile" element={<SignIn />} />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-un" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   );
 }
