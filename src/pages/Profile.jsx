73003e55291341a8bdc6d2c8e52f451142021532
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-06 22:11:59 +0100

    User sign in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const auth = getAuth();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
 }
 
 export default Profile;
