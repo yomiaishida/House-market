daafefd36558e9f5fba7c6e686d83843a8f744d3
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-08 21:29:47 +0100

    User logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,33 @@
 import { useState, useEffect } from "react";
 import { getAuth } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
   const auth = getAuth();
-  const [user, setUser] = useState(null);
+  const [formData, setformData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
-  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
