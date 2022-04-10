e4b3322f857fcaf6a79bfaa01d98c11cfab0a1ab
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-10 23:42:14 +0100

    useAuthStatus hook

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStatus } from "../hooks/useAuthStatus";
 
 const PrivateRoute = () => {
-  const loggedIn = false;
+  const { loggedIn, checkingStatus } = useAuthStatus;
 
-  return loggeIn ? <Outlet /> : <Navigate to="sign-in" />;
+  if (checkingStatus) {
+    return <h3>Loading...</h3>;
+  }
+
+  return loggedIn ? <Outlet /> : <Navigate to="sign-in" />;
 };
 
 export default PrivateRoute;
