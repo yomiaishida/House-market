3a533bd35286b268e160eb10c3d36fd6c926a64e
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-12 22:09:41 +0100

    Fix: typo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar";
-import Navbar from "./components/PrivateRoute";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const useAuthStatus = () => {
-  const [loggedIn, setLoggedIn] = useState(faolse);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
   const isMounted = useRef(true);
 
