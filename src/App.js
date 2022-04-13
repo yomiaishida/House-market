01c3d06d3a4b6a5b98ea458eef6689c578a6dde6
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-13 05:49:41 +0100

    Fix: typo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword.Jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
 
 function App() {
   return (
