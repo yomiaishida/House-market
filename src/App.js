cebd093f205dd3fce6842a0eb61baee797e05d49
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-18 03:08:07 +0100

    Fix: typo

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="profile" element={<PrivateRoute />}>
+          <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
