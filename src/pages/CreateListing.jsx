5f9c9b1ac8b709e0bf285c48bbc19650de871861
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-20 23:35:58 +0100

    Create listing final part

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -61,9 +61,35 @@ const CreateListing = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    console.log(formData);
   };
 
-  const onMutate = (e) => {};
+  const onMutate = (e) => {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+
+    // Text/Booleans/Numbers
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
 
   if (loading) {
     return <Spinner />;
