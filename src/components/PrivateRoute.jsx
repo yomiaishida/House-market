53b9c0b167842cf4439535d1a1f1dcd091ede0fd
Abayomi Aishida <yomicorperate@gmail.com>
2022-04-28 15:40:16 +0100

    Fix: finally fixed sign in error

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { useAuthStatus } from "../hooks/useAuthStatus";
 import Spinner from "./Spinner";
 
 const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus;
+  const { loggedIn, checkingStatus } = useAuthStatus();
 
   if (checkingStatus) {
     return <Spinner />;
