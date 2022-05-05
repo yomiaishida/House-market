9f4f8bfe6e22e624dfab57ceb7c7a4977371085f
Abayomi Aishida <yomicorperate@gmail.com>
2022-05-05 22:04:24 +0100

    Uploading images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownLoadURL,
+} from "firebase/storage";
+import { db } from "../firebase.config ";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
