e5fd744681d0c2862fceb21ff8a56881b5d7432a
Abayomi Aishida <yomicorperate@gmail.com>
2022-05-10 22:15:45 +0100

    Starting out contact landlord page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/contact/:landlordId" element={<Contact />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+
+function Contact() {
+  const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("Could not get landlord data");
+      }
+    };
+
+    getLandlord();
+  }, [params.landlordId]);
+  return <div>Contact</div>;
+}
+
+export default Contact;
