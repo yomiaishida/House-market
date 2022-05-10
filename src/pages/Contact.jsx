e96d725e2c26f2d12120ac63faa8c4657e0bf912
Abayomi Aishida <yomicorperate@gmail.com>
2022-05-10 22:34:17 +0100

    Finalizing contact landlord page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,7 +25,49 @@ function Contact() {
 
     getLandlord();
   }, [params.landlordId]);
-  return <div>Contact</div>;
+
+  const onChange = (e) => setMessage(e.targe.value);
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Contact Landlord</p>
+      </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contactLandlord">
+            <p className="landlordName">Contact {landlord?.name}</p>
+          </div>
+
+          <form className="messageForm">
+            <div className="messageDiv">
+              <label htmlFor="message" className="messageLabel">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="textarea"
+                value={message}
+                onChange={onChange}
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                "listingName"
+              )}&body=${message}`}
+            >
+              <button className="primaryButton" type="button">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  );
 }
 
 export default Contact;
