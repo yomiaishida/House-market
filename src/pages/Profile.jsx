b27825ab960b0832dbc7094a45e4307b60d68cdb
Abayomi Aishida <yomicorperate@gmail.com>
2022-05-17 20:25:58 +0100

    Edit listing Icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -102,6 +102,10 @@ function Profile() {
     }
   };
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`);
+  };
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -163,6 +167,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
