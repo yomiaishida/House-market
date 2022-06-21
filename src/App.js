c32dcf198ebf5de1a3d8da0f9c937070db7e26be
Abayomi Aishida <yomicorperate@gmail.com>
2022-06-21 14:07:45 +0100

    Forgot password another fix

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
+import Category from "./pages/Category";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Category from "./pages/Category";
+import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
 import Contact from "./pages/Contact";
-import EditListing from "./pages/EditListing";
-import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -31,11 +31,11 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/edit-listing/:listingId" element={<EditListing />} />
-          <Route path="/contact/:landlordId" element={<Contact />} />
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
